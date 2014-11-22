df4c5e8d0ab0562264ee3c6a900754679a3b950c
Franklin Chen <franklinchen@franklinchen.com>
2014-11-22 11:49:08 -0500

    Change tests to work around rustdoc limitation on extern crate.

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,20 @@ extern crate core;
 /// We provide it as `f64` for now.
 ///
 /// ```rust{.example}
-/// assert!(tau::TAU / 2.0 == core::f64::consts::PI);
+/// use std::num::FloatMath;
+/// use tau::TAU;
+/// assert_eq!(TAU.cos(), 1.0);
 /// ```
 #[unstable = "still waiting for associated constants"]
 pub const TAU: f64 = core::f64::consts::PI_2;
+
+#[cfg(test)]
+mod test {
+    extern crate core;
+    use super::TAU;
+
+    #[test]
+    fn pi_is_a_derived_constant() {
+        assert_eq!(core::f64::consts::PI, TAU / 2.0);
+    }
+}
