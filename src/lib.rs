d1878de9ca6e367f80eb256e68c50bec7274fb5f
Franklin Chen <franklinchen@franklinchen.com>
2015-04-23 13:50:47 -0400

    Use std instead of core.

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@
 
 #![feature(core)]
 
-extern crate core;
+use std::f64::consts::PI_2;
 
 /// The mathematical constant [tau](http://tauday.com/),
 /// where pi is defined as tau/2.
@@ -13,16 +13,15 @@ extern crate core;
 /// use tau::TAU;
 /// assert_eq!(TAU.cos(), 1.0);
 /// ```
-#[unstable = "still waiting for associated constants"]
-pub const TAU: f64 = core::f64::consts::PI_2;
+pub const TAU: f64 = PI_2;
 
 #[cfg(test)]
 mod test {
-    extern crate core;
     use super::TAU;
+    use std::f64::consts::PI;
 
     #[test]
     fn pi_is_a_derived_constant() {
-        assert_eq!(core::f64::consts::PI, TAU / 2.0);
+        assert_eq!(PI, TAU / 2.0);
     }
 }
